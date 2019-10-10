12145acc46068f181d282225288b946bbad61bc4
Jacob Svensson <oi17jsn@cs.umu.se>
2019-10-10 14:28:33 +0200

    Amazing progress!! We can now READ THE FILES and PUT THEM IN SUITABLE DATASTRUCTURES

--- digits.py
+++ digits.py
@@ -36,16 +36,25 @@ class ReadFile:
 
         return label
 
-
+# Vill vi eventuellt lagra labels och images i samma struktur med en kry/value?
 if __name__ == "__main__":
 
-    training = ReadFile(sys.argv[1])
-    temp = training.read_images()
-    print(temp[1])
+    all_images = ReadFile(sys.argv[1])
+    temp = all_images.read_images()
 
-    label = ReadFile(sys.argv[2])
-    temp2 = label.read_label()
-    print(temp2[0])
+    all_label = ReadFile(sys.argv[2])
+    temp2 = all_label.read_label()
+
+    # Dela listorna i två delar, training och test, rekommenderat training = 75 / test = 25 som start.
+    # Måste hålla koll så att labels och images är i samma ordning. Tar vi plats 10 i en lista
+    # måste vi ävem göra det i andra så de har samma plats. Värderna måste shufflas runt,
+    # borde bli samma sak med att ta random objekt och flytta till nya listorna.
+
+    # För training, skapa ny array som initaliseras med random weights mellan - 0.05 till 0.05. Blir lika stor
+    # som trainigs storlek. Sedan vill vi skicka in en pixel och dess weight i en
+    # "activation function" samt en label compare ( se om den är rätt eller inte)
+    # för att träna och uppdatera med en ny weight.
+
 
 # TODO: From where will the "input" come from?
 #weights = [0.1 * random() - 0.05 for x in range(images)]
