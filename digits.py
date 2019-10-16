2a3904d3e3910b52aceffc40d5465820138546a4
William Larsson <oi17wln@cs.umu.se>
2019-10-16 10:52:03 +0200

    comments, comments, comments

--- digits.py
+++ digits.py
@@ -4,37 +4,66 @@ import sys
 
 
 class Network:
+    """
+    A class representing each neuron in the neural network.
+    """
+
     def __init__(self, label, size):
+        """
+        Construct a neuron with a number it should specialize on.
+        Also create an array of random weights for between the neuron and following node.
+        :param label: The number the specialize on.
+        :param size: The number of pixels in an image.
+        """
         self.label = label
         self.weights = np.array([0.1 * random() - 0.05 for _ in range(size)])
 
     def calculate_error(self, label, a):
+        """
+        Calculate the error between the desired output y and the activation a of the current neuron.
+        :param label: The label representing the real number of the current image.
+        :param a: The activation function output.
+        :return: The error value.
+        """
         y = 1 if self.label == label else -1
         return y - a
 
     def calculate_new_weight(self, error, img, learning_rate):
+        """
+        Calculates the new weight between the neuron and the output node.
+        :param error: The error between the desired output y and the activation function
+        :param img: An image array with all its pixels
+        :param learning_rate: Balance how much the weight is updated
+        :return: Updated weight between the neuron and the following node.
+        """
         for i in range(len(self.weights)):
             self.weights[i] = self.weights[i] + img[i] * error * learning_rate
 
     def activation_function(self, dot):
+        """
+        Maps the neurons inputs to corresponding output.
+        :param dot: The dot product of the neurons weights and the pixel values.
+        :return: A Value representing how active the neuron should be for this particular input.
+        """
         return np.tanh(dot)
 
     def dot_product(self, pixels):
+        """
+        Calculate the dot product between all pixels and weights for an image.
+        :param pixels: Array of pixels in an image
+        :return: The dot product sum
+        """
         return np.dot(self.weights, pixels)
 
 
-# Generate a list with random weights. Size is the size of every picture
-# We want to create one for every network, so if we have 4,7,8 and 9 we need 4 lists with random weights.
-# They will update after every "run" with a picture in a network.
-def generate_weight_list(size):
-    weights = [0.1 * random() - 0.05 for x in range(size)]
-    weights = np.array(weights)
-    return weights
 
-
-# A FUNCTION FOR SPLITTING A LIST IN A DESIRED WAY
-# RATIO IS A NUMBER 0-1 AND SETS THE "MID POINT" WHERE THE LIST SHOULD SPLIT
 def list_splitter(list_to_split, ratio):
+    """
+    Split the list of all images into test and training sub sets.
+    :param list_to_split: Numpy array of all the images of handwritten numbers
+    :param ratio: The proportion between the test and training sub set. Sets the midpoint for the split.
+    :return: A list of testing images and a list of training images
+    """
     elements = len(list_to_split)
     middle = int(elements * ratio)
     return [list_to_split[:middle], list_to_split[middle:]]
@@ -43,6 +72,13 @@ def list_splitter(list_to_split, ratio):
 # A FUNCTION FOR SHUFFLING THE OBJECTS IN TWO LISTS THE SAME WAY.
 # AKA CORRECT LABEL FOR THE IMAGE
 def list_shuffler(image_list_to_shuffle, label_to_shuffle):
+    """
+    Takes in two lists of equal size and shuffles each list randomly. However both lists
+    are shuffled in the same random order.
+    :param image_list_to_shuffle: List of images
+    :param label_to_shuffle: List of labels for the images.
+    :return:
+    """
     temp_zip = list(zip(image_list_to_shuffle, label_to_shuffle))
     np.random.shuffle(temp_zip)
     shuffled_image_list, shuffled_label_list = zip(*temp_zip)
